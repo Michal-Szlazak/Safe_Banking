a88d95afb1206791e88e0f20b6b06ad6482d1dd4
szlazakm <01169634@pw.edu.pl>
2023-12-15 10:52:42 +0100

    login component

--- frontend/app/src/app/login/login.component.ts
+++ frontend/app/src/app/login/login.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { LoginService } from "../shared/services/login.service";
+import {AbstractControlOptions, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {RegisterService} from "../shared/services/register.service";
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
-  constructor(private loginService: LoginService) {}
+  loginForm: FormGroup;
 
-  loginData = {
-    email: '',
-    password: ''
-  };
+  constructor(private loginService: LoginService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.maxLength(20), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]]
+    });
+  }
 
   onSubmit() {
-    this.loginService.login(this.loginData.email, this.loginData.password).subscribe(
-      response => console.log(response)
-    );
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.loginService.login(email, password);
+    }
   }
 }
