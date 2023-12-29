350b8c8d7627e1268a3d6cc3d1c4ba92bbe9f76d
szlazakm <michal.szlazak77@gmail.com>
2023-12-29 12:25:42 +0100

    Security config for OPTIONS requests sent by browser

--- backend/gateway-service/src/main/java/safe/bank/app/apigateway/config/WebSecurity.java
+++ backend/gateway-service/src/main/java/safe/bank/app/apigateway/config/WebSecurity.java
@@ -2,10 +2,17 @@ package safe.bank.app.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 
 @Configuration
@@ -17,6 +24,7 @@ public class WebSecurity {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorize -> authorize
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .pathMatchers("/auth/user/public/**").permitAll()
                         .anyExchange().authenticated()
                 )
