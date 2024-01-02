8e2a256d249aa32c9d3a156a8c692d3c98c090a7
szlazakm <01169634@pw.edu.pl>
2024-01-02 14:28:36 +0100

    Blocking user after 5 login tries.

--- frontend/app/src/app/login/login.component.ts
+++ frontend/app/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoginService } from "../shared/services/login.service";
 import {AbstractControlOptions, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,13 @@ export class LoginComponent {
 
   loginForm: FormGroup;
   loginError = false;
+
+  loginAttempts: number = 0;
+  maxLoginAttempts: number = 5;
+  isUserBlocked: boolean = false;
+  remainingTimeSubscription: Subscription | undefined;
+  remainingTime: number = 60;
+  blockDuration = 60 * 1000;
 
   constructor(private loginService: LoginService, private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
@@ -31,8 +39,13 @@ export class LoginComponent {
             // Redirect or perform other actions after successful login
           } else {
             console.error('Login failed');
+            this.loginAttempts++;
             this.loginError = true;
-            // Handle login failure (show error message, etc.)
+
+            console.log(this.loginAttempts);
+            if(this.loginAttempts >= this.maxLoginAttempts) {
+              this.blockUser();
+            }
           }
         },
         error: (err) => {
@@ -41,5 +54,61 @@ export class LoginComponent {
         }
       });
     }
+  }
+
+  private calculateRemainingTime(): void {
+    this.remainingTime--;
+  }
+
+  private blockUser(): void {
+    this.isUserBlocked = true;
+    this.remainingTime = 60;
+    this.calculateRemainingTime(); // Trigger initial calculation
+    this.startRemainingTimeUpdate();
+    this.toggleControls();
+
+    setTimeout(() => {
+      this.unlockUser();
+      this.stopRemainingTimeUpdate();
+    }, this.blockDuration);
+  }
+
+  private unlockUser(): void {
+    this.isUserBlocked = false;
+    this.loginAttempts = 0;
+    this.toggleControls();
+  }
+
+  private startRemainingTimeUpdate(): void {
+    // Update remaining time every second
+    this.remainingTimeSubscription = interval(1000).subscribe(() => {
+      this.calculateRemainingTime();
+    });
+  }
+
+  private stopRemainingTimeUpdate(): void {
+    // Stop the interval when it's no longer needed
+    if (this.remainingTimeSubscription) {
+      this.remainingTimeSubscription.unsubscribe();
+    }
+  }
+
+  toggleControls(): void {
+    if (this.isUserBlocked) {
+      // this.loginForm.get('email')?.disable();
+      // this.loginForm.get('password')?.disable();
+      // this.loginForm.get('button')?.disable();
+      this.loginForm.disable();
+    } else {
+      // this.loginForm.get('email')?.enable();
+      // this.loginForm.get('password')?.enable();
+      // this.loginForm.get('button')?.enable();
+      this.loginForm.enable();
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Ensure to unsubscribe when the component is destroyed
+    this.stopRemainingTimeUpdate();
   }
 }
